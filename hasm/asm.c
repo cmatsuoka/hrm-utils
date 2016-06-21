12129de28441aed481d0e09f7559a044a855949e
Claudio Matsuoka <cmatsuoka@gmail.com>
2016-06-21 13:53:53 -0300

    Add assembler pass 2
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- hasm/asm.c
+++ hasm/asm.c
@@ -2,12 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <getopt.h>
+#include <stdarg.h>
 #include "hrm.h"
 
 #define LINE_SIZE 80
+#define SYMBOL_SIZE 16
+#define SYMTABLE_SIZE HRM_TEXTSIZE
 
 struct symbol {
-	char *name;
+	char name[SYMBOL_SIZE];
 	uint8_t addr;
 };
 
@@ -33,11 +36,24 @@ static struct instruction instruction[] = {
 	{ NULL,		0, 0, 0		}
 };
 
-static struct symbol symtable[HRM_TEXTSIZE];
+static struct symbol symtable[SYMTABLE_SIZE];
 static int st_index = 0;
 static int addr;
 static uint8_t bin[HRM_TEXTSIZE];
 
+
+static void panic(char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	fprintf(stderr, "Error: ");
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
+	va_end(ap);
+
+	exit(EXIT_FAILURE);
+}
 
 static void parse_line(char *l, char **label, char **instr, char **arg)
 {
@@ -67,9 +83,58 @@ static void parse_line(char *l, char **label, char **instr, char **arg)
 
 static void add_symtable(char *name, int addr)
 {
-	symtable[st_index].name = strdup(name);
+	if (st_index >= SYMTABLE_SIZE) {
+		panic("too many symbols");
+	}
+
+	/* check if symbol already in table */
+	for (int i = 0; i < st_index; i++) {
+		if (!strcmp(symtable[i].name, name)) {
+			panic("duplicate symbol \"%s\"", name);
+		} 
+	}
+
+	strncpy(symtable[st_index].name, name, SYMBOL_SIZE - 1);
 	symtable[st_index].addr = addr;
 	st_index++;
+}
+
+static char *read_line(char *l, size_t n, FILE *f)
+{
+	char *t;
+
+	if ((l = fgets(l, LINE_SIZE, f)) == NULL) {
+		return NULL;
+	}
+
+	/* remove trailing newline */
+	int len = strlen(l);
+	while (len > 0 && (l[len - 1] == '\n' || l[len - 1] == '\r')) {
+		l[len - 1] = 0;
+		len--;
+	}
+
+	/* remove comments */
+	if ((t = strstr(l, "--")) != NULL) {
+		*t = 0;
+	}
+
+	/* remove spaces */
+	for (; *l == ' ' || *l == '\t'; l++);
+
+	return l;
+}
+
+static uint8_t get_argument(char *arg)
+{
+	uint8_t val;
+
+	if (arg[0] == '[') {
+		/* indirect addressing */
+		return atoi(arg + 1) | 0x10;
+	} else {
+		return atoi(arg);	
+	}
 }
 
 static void pass1(FILE *f)
@@ -77,34 +142,16 @@ static void pass1(FILE *f)
 	char line[LINE_SIZE];
 	char *l, *label, *instr, *arg;
 
-	printf("PASS 1: ");
+	printf("\nPASS 1: ");
 
 	while (42) {
 		char *t;
 
-		printf(".");
-		fflush(stdout);
-
-		if (fgets(line, LINE_SIZE, f) == NULL) {
+		if ((l = read_line(line, LINE_SIZE, f)) == NULL) {
 			break;
 		}
 
-		/* remove trailing newline */
-		int len = strlen(line);
-		while (len > 0 && (line[len - 1] == '\n' || line[len - 1] == '\r')) {
-			line[len - 1] = 0;
-			len--;
-		}
-
-		/* remove comments */
-		if ((t = strstr(line, "--")) != NULL) {
-			*t = 0;
-		}
-
-		/* remove spaces */
-		for (l = line; *l == ' ' || *l == '\t'; l++);
-
-		/* remove empty lines */
+		/* ignore empty lines */
 		if (*l == 0 || *l == '\n') {
 			continue;
 		}
@@ -119,6 +166,7 @@ static void pass1(FILE *f)
 		for (ins = instruction; ins->ins; ins++) {
 			if (!strcmp(instr, ins->ins)) {
 				addr += ins->size;
+				break;
 			}
 		}
 	}
@@ -131,7 +179,54 @@ static void pass1(FILE *f)
 
 static void pass2(FILE *f)
 {
-	printf("PASS 2:\n");
+	char line[LINE_SIZE];
+	char *l, *label, *instr, *arg;
+
+	printf("\nPASS 2: ");
+
+	while (42) {
+		char *t;
+
+		if ((l = read_line(line, LINE_SIZE, f)) == NULL) {
+			break;
+		}
+
+		/* ignore empty lines */
+		if (*l == 0 || *l == '\n') {
+			continue;
+		}
+
+		parse_line(l, &label, &instr, &arg);
+
+		if (*label) {
+			printf("            %s:\n", label);
+
+		}
+		if (*instr) {
+			struct instruction *ins;
+			for (ins = instruction; ins->ins; ins++) {
+				if (!strcmp(instr, ins->ins)) {
+					uint8_t op = ins->opcode;
+					printf("%3d  ", addr);
+					if (ins->arg == 0) {
+						bin[addr] = op;
+						printf("%02X         %-8.8s", op, instr);
+					} else if (ins->arg == 1) {
+						op |= get_argument(arg);
+						bin[addr] = op;
+						printf("%02X         %-8.8s %s", op, instr, arg);
+					} else if (ins->arg == 2) {
+						bin[addr] = op;
+						bin[addr + 1] = 0x55;
+						printf("%02X %02X      %-8.8s %s", op, bin[addr + 1], instr, arg);
+					}
+					printf("\n");
+					addr += ins->size;
+					break;
+				}
+			}
+		}
+	}
 }
 
 static void usage(char *cmd)
@@ -169,6 +264,7 @@ int main(int argc, char **argv)
 
 	/* pass 2: assemble code */
 	fseek(f, 0, SEEK_SET);
+	addr = 0;
 	pass2(f);
 
 	/* save object code */
